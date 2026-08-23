0166cefd0b129d920948045b63ad8ad5d0d35703
Bhavik Javiya <javiyabhavik24@gmail.com>
2026-08-23 15:30:13 +0530

    Basic Flyway practice (Versioned script eg.-> V1__add_table.sql)

---
+++ SpringJDBC/04-Flyway-practice/src/main/resources/db/migration/V1__Create_student_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE student (id INT PRIMARY KEY, name VARCHAR(20) NOT NULL, city VARCHAR(20));
+INSERT INTO student (id, name, city) VALUES
+                                         (101, 'AAA', 'Pune'),
+                                         (102, 'BBB', 'Mumbai'),
+                                         (103, 'CCC', 'Pune'),
+                                         (104, 'DDD', 'Nashik');
---
+++ SpringJDBC/04-Flyway-practice/src/main/resources/db/migration/V2__Add_percentage_column.sql
@@ -0,0 +1,1 @@
+ALTER TABLE student ADD COLUMN percentage NUMERIC(5,2);
