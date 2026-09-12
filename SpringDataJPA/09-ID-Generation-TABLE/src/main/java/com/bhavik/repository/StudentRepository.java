a4299e23ae03817636accd128ad13ece86b4f976
Bhavik Javiya <javiyabhavik24@gmail.com>
2026-09-12 17:25:24 +0530

    Update StudentRepository to UUID

--- SpringDataJPA/09-ID-Generation-TABLE/src/main/java/com/bhavik/repository/StudentRepository.java
+++ SpringDataJPA/09-ID-Generation-TABLE/src/main/java/com/bhavik/repository/StudentRepository.java
@@ -4,6 +4,8 @@ import com.bhavik.entity.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface StudentRepository extends CrudRepository<Student, Integer> {
+public interface StudentRepository extends CrudRepository<Student, UUID> {
 }
